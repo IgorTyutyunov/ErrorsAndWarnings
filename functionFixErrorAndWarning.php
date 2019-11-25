31d0ffde6af6c19e7a99cf2eb36edf09cd830e9b
IgorTyutyunov <53127430+IgorTyutyunov@users.noreply.github.com>
2019-11-25 17:39:36 +0300

    Исправлена ошибка с нулевым количеством предупреждений

--- functionFixErrorAndWarning.php
+++ functionFixErrorAndWarning.php
@@ -1,21 +1,36 @@
 <?php 
  function fixErrorAndWarning($error,$warning){
+	 if($error%2!=0 && $warning==0)
+		 return -1;
+	 
 	 $arr=array();
+	 $countStepA=0;
 	 $countStepB=0;
 	 $countStepC=0;
 	 $totalStep=0;
+	 
+	 if(($warning%2==0)&&(($error+(int)($warning/2))%2!=0)){ 
+		$warning++;
+		$totalStep++;
+		$countStepA++;
+	 }
+	 
 	 $countStepB=(int)($warning/2);//1. Количеству шагов, типа B, присваиваем целую часть от деления на 2 количества предупреждений.
-	 $warning=$warning%2;//Количество предупреждений приравниваем к остатку от деления количества предупреждений на два.
+	 $warning=$warning%2;//Количество предупреждений приравниваем к остатку от деления количества предупреждений на два.	 
 	 $error=$error+$countStepB;//Увеличиваем количество ошибок на количество шагов типа B.
 	 $countStepC=(int)($error/2);//увеличиваем количетво шагов типа C на целую часть от деления количества ошибок.
 	 $error=$error%2;//количество ошибок приравниваем к остатку от деления количества ошибок на два.
-	 $totalStep=$countStepB+$countStepC;//Суммируем количество шагов типа B и C и получаем общее количество шагов.
+	 $totalStep+=$countStepB+$countStepC;//Суммируем количество шагов типа B и C и получаем общее количество шагов.
+	 
+	 
+	 
  if($error==0 && $warning==1){
 	 $totalStep+=6;
 	 $arr[0]=$totalStep;
 	 $arr[1]=$countStepB;
 	 $arr[2]=$countStepC;
-	 $arr[3]='A>>B>>A>>A>>B>>C=>избавились от ошибок и предупреждений!';
+	 $arr[3]='A>>A>>B>>A>>B>>C=>избавились от ошибок и предупреждений!';
+	 $arr[4]=$countStepA;
 	 return $arr;
  }
  if($error==1 && $warning==1){
@@ -24,6 +39,7 @@
 	 $arr[1]=$countStepB;
 	 $arr[2]=$countStepC;
 	 $arr[3]='A>>B>>C=>избавились от ошибок и предупреждений!';
+	 $arr[4]=$countStepA;
 	 return $arr;
  }
  if($error==0 && $warning==0){
@@ -31,15 +47,16 @@
 	 $arr[1]=$countStepB;
 	 $arr[2]=$countStepC;
 	 $arr[3]='=>избавились от ошибок и предупреждений!';
+	 $arr[4]=$countStepA;
 	 return $arr;
  }
- if($error==1 && $warning==0){
-	 $totalStep+=4;
-	 $arr[0]=$totalStep;
-	 $arr[1]=$countStepB;
-	 $arr[2]=$countStepC;
-	 $arr[3]='A>>A>>B>>C=>избавились от ошибок и предупреждений!';
-	 return $arr;
- }
+ // if($error==1 && $warning==0){
+	 // $totalStep+=4;
+	 // $arr[0]=$totalStep;
+	 // $arr[1]=$countStepB;
+	 // $arr[2]=$countStepC;
+	 // $arr[3]='A>>A>>B>>C=>избавились от ошибок и предупреждений!';
+	 // return $arr;
+ // }
  }
 	?>
--- index.php
+++ index.php
@@ -26,10 +26,14 @@ if(isset($_POST['submitDiscover'])){
 	$war=$_POST[textWarning];
 	if((is_numeric($er)&&is_numeric($war))&&($er>=0 && $war>=0)){
 		$arr=fixErrorAndWarning($er,$war);
-	echo "Ошибок: ".$er."<br>";
-	echo "Предупреждений: ".$war."<br>";
-	echo "Количество шагов, за которое можно исправить ошибки и предупреждения: ".$arr[0]."<br>";
-	echo "B*$arr[1]>>С*$arr[2]>>$arr[3]"."<br>";
+		if($arr!=-1){
+			echo "Ошибок: ".$er."<br>";
+			echo "Предупреждений: ".$war."<br>";
+			echo "Количество шагов, за которое можно исправить ошибки и предупреждения: ".$arr[0]."<br>";
+			echo "A*$arr[4]>>B*$arr[1]>>С*$arr[2]>>$arr[3]"."<br>";
+		}
+		else 
+			echo "Пете невозможно избавиться от ошибок и предупреждений: ".$arr;
 	}
 	else
 		echo "Введены некорректные данные";
